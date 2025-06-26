28b0f52fd9fad466226b61008cbf002888a87096
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-06-26 22:33:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, render_template
+from transformers import pipeline
+
+app = Flask(__name__)
+summarizer = pipeline("summarization")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    text = data.get("text", "")
+    if not text.strip():
+        return jsonify({"summary": "Please enter valid text to summarize."})
+    summary = summarizer(text, max_length=60, min_length=20, do_sample=False)
+    return jsonify({"summary": summary[0]['summary_text']})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
